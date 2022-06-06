e09577ff1e00b5d39187bd302f1740fbf83ce471
ParveendThaku <parveend1234@gmail.com>
2022-06-06 23:34:30 +0530

    testing

---
+++ Module 3/excel/script.js
@@ -0,0 +1,14 @@
+let cellscontent = document.querySelector(".cells-content");
+
+function initcells(){
+    let cell = "";
+    for (let i=0; i<100; i++){
+        cell+="<div class = 'row'>";
+        // column
+        for(let j=0; j<26; j++){
+            cell+="<div class = 'cell'>CELL</div>"
+        }
+        cell+="</div>"
+    }
+    cellscontent.innerHTML = cell;
+}
