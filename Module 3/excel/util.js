0451e619c80b4e4721e72df9a385bbb45474c913
ParveendThaku <parveend1234@gmail.com>
2022-06-09 13:30:36 +0530

    children update dfs

--- Module 3/excel/util.js
+++ Module 3/excel/util.js
@@ -8,13 +8,31 @@ function solveFormula(formula){
             let {rowId,colId} = getRowIdColIdFromAddress(formulaComp);
             let cellObject = db[rowId][colId];
             let value = cellObject.value;
+            if(selfCellObject)
+                cellObject.children.push(selfCellObject.name);
+            console.log(cellObject);    
             formula = formula.replace(formulaComp,value);
         }
     }
+    // formula -> 2 * 3 + 4 - 3
     let computedValue = eval(formula);
     return computedValue; 
 }
 
+function updateChildren(cellObject){
+    for(let i=0;i<cellObject.children.length;i++){
+        let childName = cellObject.children[i];
+        let {rowId,colId} = getRowIdColIdFromAddress(childName);
+        let childCellObject = db[rowId][colId];
+        let newValue = solveFormula(childCellObject.formula);
+        //update UI
+        let cellUI = document.querySelector(`div[rowid ='${rowId}'][colid='${colId}']`); 
+        cellUI.textContent = newValue;
+        // update db
+        childCellObject.value = newValue;
+        updateChildren(childCellObject)
+    }
+}
 
 function getRowIdColIdFromElement(element){
     let rowId = element.getAttribute("rowid");
