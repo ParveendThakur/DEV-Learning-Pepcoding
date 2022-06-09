db44abdb8340e3c1e670074cd6954eb977199da6
ParveendThaku <parveend1234@gmail.com>
2022-06-09 20:23:46 +0530

    bold italic underline logic

--- Module 3/excel/init.js
+++ Module 3/excel/init.js
@@ -47,7 +47,8 @@ function initDb(){
                 formula:"",
                 childern:[],
                 parent:[],
-                visited:false
+                visited:false,
+                fontStyle:{bold:false,italic:false,underline:false}
             }
             row.push(cellObject);
         }
--- Module 3/excel/script.js
+++ Module 3/excel/script.js
@@ -25,6 +25,15 @@ for(let i=0;i<allCells.length;i++){
         addressInput.value = address;
         // Update UI
         formulaInput.value = cellObject.formula;
+
+        cellObject.fontStyle.bold?document.querySelector(".bold").classList.add("active-font-style"):
+        document.querySelector(".bold").remove("active-font-style");
+
+        cellObject.fontStyle.italic?document.querySelector(".italic").classList.add("active-font-style"):
+        document.querySelector(".italic").remove("active-font-style");
+
+        cellObject.fontStyle.underline?document.querySelector(".underline").classList.add("active-font-style"):
+        document.querySelector(".underline").remove("active-font-style");
     })
 
     allCells[i].addEventListener("blur",function(e){
