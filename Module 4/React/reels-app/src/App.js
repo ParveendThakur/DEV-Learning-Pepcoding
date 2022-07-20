da0e710c58f7401effd3a1777521c0f2774a99b6
ParveendThakur <parveend1234@gmail.com>
2022-07-20 20:09:34 +0530

    import the file -- components

--- Module 4/React/reels-app/src/App.js
+++ Module 4/React/reels-app/src/App.js
@@ -1,6 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// npm install react-router-dom
 
+import './App.css';
+import Feed from "./components/Feed"
+import Login from "./components/login"
+import PageNotFound from "./components/PageNotFound"
+import Profile from "./components/Profile"
+import Signup from "./components/Signup"
+import {Switch,Route} from "react-router-dom"
 function App() {
   return (
     
