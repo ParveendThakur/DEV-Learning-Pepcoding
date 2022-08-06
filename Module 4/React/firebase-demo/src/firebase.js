64811ff4014452a89bcb03775d515c9d779eb0a3
ParveendThakur <parveend1234@gmail.com>
2022-08-06 23:53:11 +0530

    "Firebase setup"

---
+++ Module 4/React/firebase-demo/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3vQVyuGPxesEaRXPXGRBWQVbMLMz0L38",
+  authDomain: "class-demo-bdf7c.firebaseapp.com",
+  projectId: "class-demo-bdf7c",
+  storageBucket: "class-demo-bdf7c.appspot.com",
+  messagingSenderId: "886173863201",
+  appId: "1:886173863201:web:a58fca6257f522c82d4260"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
