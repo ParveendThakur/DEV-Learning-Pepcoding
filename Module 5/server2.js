ff3d1ca3dd8d6f911f270ad28c169ba82bdccecf
ParveendThakur <parveend1234@gmail.com>
2022-08-17 16:34:59 +0530

    Case --1 ---done

--- Module 5/server2.js
+++ Module 5/server2.js
@@ -2,17 +2,18 @@ const express = require("express");
 
 const app = express();
 
-
 // case :1 
 
-app.get("/simple",function(req,res){
-    res.send("Simple output 1")
-})
+// app.get("/simple",function(req,res){
+//     res.send("Simple output 1")
+// })
 
-app.get("/simple",function(req,res){
-    res.send("Simple Output 2");
-})
+// app.get("/simple",function(req,res){
+//     res.send("Simple Output 2");
+// })
 
-app.get("/simple",function(req,res){
-    res.send("simple output 3");
-})
+// app.get("/simple",function(req,res){
+//     res.send("simple output 3");
+// })
+
+
