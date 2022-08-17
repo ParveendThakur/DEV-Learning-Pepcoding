3f91801ae178dbed2926ae1471b3adf936bf47c1
ParveendThakur <parveend1234@gmail.com>
2022-08-17 16:41:44 +0530

    CASE 2(app.use--app.get--app.patch--app.delete.....  )

--- Module 5/server2.js
+++ Module 5/server2.js
@@ -20,6 +20,21 @@ const app = express();
 
 //case 2
 
+app.use(function(req,res){
+    res.send("Use will always run");
+})
+
+app.get("/simple",function(req,res){
+    res.send("hello from get");
+})
+
+app.patch("/simple",function(req,res){
+    res.send("hello from patch");
+})
+
+app.delete("/simple",function(req,res){
+    res.send("hello from delete");
+})
 
 
 
