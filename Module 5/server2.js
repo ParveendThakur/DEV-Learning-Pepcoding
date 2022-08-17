0ddbaf93086f6d033300d8ac5ad8661809c15d63
ParveendThakur <parveend1234@gmail.com>
2022-08-17 16:32:12 +0530

    case 1 --- simple---- app.get--- Simple output 1

--- Module 5/server2.js
+++ Module 5/server2.js
@@ -2,14 +2,17 @@ const express = require("express");
 
 const app = express();
 
-// case :1
+
+// case :1 
 
 app.get("/simple",function(req,res){
     res.send("Simple output 1")
 })
 
+app.get("/simple",function(req,res){
+    res.send("Simple Output 2");
+})
 
-
-app.listen(3000,function(){
-    console.log("server is started at 3000");
+app.get("/simple",function(req,res){
+    res.send("simple output 3");
 })
