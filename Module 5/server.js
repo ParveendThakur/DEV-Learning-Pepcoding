1dec77f52395d3e38647cca83de5fbf6167a6a9e
ParveendThakur <parveend1234@gmail.com>
2022-08-17 12:40:01 +0530

    //get karna hai data from sayHello----saybye

--- Module 5/server.js
+++ Module 5/server.js
@@ -5,9 +5,16 @@ const express = require("express")
 
 const app = express();
 
-app.get("/",function(req,res){
+//get karna hai data from sayHello
+app.get("/sayhello",function(req,res){
     res.send("Hello from server");
 })
+
+//get kana hai data from sayBye
+app.get("/sayBye",function(req,res){
+    res.end("Bye");
+})
+
 
 app.listen(3000,function(){
     console.log("server started at port 3000")
