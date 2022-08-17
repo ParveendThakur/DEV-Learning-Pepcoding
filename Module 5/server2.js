bc6b93f668fd59e026eb0205d407353413739005
ParveendThakur <parveend1234@gmail.com>
2022-08-17 16:42:49 +0530

    Case 2 --done

--- Module 5/server2.js
+++ Module 5/server2.js
@@ -20,21 +20,22 @@ const app = express();
 
 //case 2
 
-app.use(function(req,res){
-    res.send("Use will always run");
-})
+// app.use(function(req,res){
+//     res.send("Use will always run");
+// })
 
-app.get("/simple",function(req,res){
-    res.send("hello from get");
-})
+// app.get("/simple",function(req,res){
+//     res.send("hello from get");
+// })
 
-app.patch("/simple",function(req,res){
-    res.send("hello from patch");
-})
+// app.patch("/simple",function(req,res){
+//     res.send("hello from patch");
+// })
 
-app.delete("/simple",function(req,res){
-    res.send("hello from delete");
-})
+// app.delete("/simple",function(req,res){
+//     res.send("hello from delete");
+// })
+
 
 
 
