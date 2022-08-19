9117a6112c5aa0b9a14b1fd7bd9e52bf1b2d962f
ParveendThakur <parveend1234@gmail.com>
2022-08-19 16:16:04 +0530

    update

--- Module 5/Food App/secrets.js
+++
@@ -1,1 +0,0 @@
-export const password = "cnHMOY9xsKcTgj8w"
--- Module 5/Food App/userModel.js
+++ Module 5/Food App/userModel.js
@@ -1,10 +1,8 @@
 //tech knowledge
 //(scheme) -> set of features and rules a certain entity should follow
+const mongodb = require(`mongoose`)
 
-import password from "./secrets"
-const mongodb = require("mongoose")
-
-let dblink = `mongodb+srv://dbUser:${password}@cluster0.ovod3sb.mongodb.net/?retryWrites=true&w=majority`
+let dblink = `mongodb+srv://dbUser:9YrdlRoCdYPC3aY1@cluster0.ovod3sb.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.connect(dbLink).then(function(){
     console.log("connected");
