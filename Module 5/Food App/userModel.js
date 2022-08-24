a99737aec649a66036351d2267f92fd4f42c3967
ParveendThakur <parveend1234@gmail.com>
2022-08-24 16:17:00 +0530

    ----aug 16 start---

--- Module 5/Food App/userModel.js
+++ Module 5/Food App/userModel.js
@@ -2,7 +2,8 @@
 //(scheme) -> set of features and rules a certain entity should follow
 const mongodb = require(`mongoose`)
 
-let dblink = `mongodb+srv://dbUser:9YrdlRoCdYPC3aY1@cluster0.ovod3sb.mongodb.net/?retryWrites=true&w=majority`
+// let dblink = `mongodb+srv://dbUser:9YrdlRoCdYPC3aY1@cluster0.ovod3sb.mongodb.net/?retryWrites=true&w=majority`
+let dblink = `mongodb+srv://dbUser:MjAQyVa3DVlYkpKY@cluster0.ovod3sb.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.connect(dbLink).then(function(){
     console.log("connected");
